8ca9e1042f898df6c2b14d29ca4edad58dbd2d5c
Agos95 <agostini.federico.95@gmail.com>
2024-08-28 15:51:14 +0200

    ex. 3.8

--- part_03/phonebook_backend/index.js
+++ part_03/phonebook_backend/index.js
@@ -1,7 +1,11 @@
 import express from "express"
+import morgan from "morgan"
+
+morgan.token("body", (req) => JSON.stringify(req.body))
 
 const app = express()
 app.use(express.json())
+app.use(morgan(":method :url :status :res[content-length] - :response-time ms - :body"))
 
 let persons = [
     {
